8708052c42051c794e41b8a23004aaf13052a6d9
ganeshdots <ganeshdots@gmail.com>
2017-07-05 23:36:54 +0530

    onetwothree

---
+++ TestProj/src/main/java/testProj/TestProj/App.java
@@ -0,0 +1,13 @@
+package testProj.TestProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
